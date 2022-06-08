e4061a42d7576732a2c6031daf535cf8772408a3
Jason Duong <reddkingdom@csu.fullerton.edu>
2022-06-08 11:14:56 -0700

    Initial Commit

---
+++ CarbonFoodPrint/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarbonFoodPrint
+//
+//  Created by Jason Duong on 6/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
