cfb438b71f6cee19cd929834d8f1ec4c5eb9d20a
Jason Duong <reddkingdom@csu.fullerton.edu>
2022-06-11 17:49:28 -0700

    Added Design View

--- CarbonFoodPrint/DisplayView.swift
+++ CarbonFoodPrint/DisplayView.swift
@@ -8,28 +8,53 @@
 import Foundation
 import SwiftUI
 
+enum Impact {
+    case Carbon
+    case Water
+    case Soil
+    case Antibiotics
+}
+
 struct DisplayView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Text("Reduced Carbon Emissions")
-                    .padding(0)
-                Text("60%")
-            }
-            HStack {
-                Text("Water Saved")
-                    .padding()
-                Text("30%")
-            }
-            HStack {
-                Text("Antibiotics Prevented")
-                    .padding()
-                Text("10%")
-            }
-            HStack {
-                Text("Agriculture Improvement")
-                    .padding()
-                Text("25%")
+        VStack(spacing: 10) {
+           Text("Your Impact on the World")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.brown)
+            ImpactBoxView(factor: .Carbon, percentage: 60)
+            ImpactBoxView(factor: .Water, percentage: 15)
+            ImpactBoxView(factor: .Soil, percentage: 20)
+            ImpactBoxView(factor: .Antibiotics, percentage: 5)
+        }
+    }
+}
+
+struct ImpactBoxView: View {
+    // Constructs Impact Boxes
+    var factor: Impact
+    var percentage: UInt
+
+    init(factor: Impact, percentage: UInt) {
+        self.factor = factor
+        self.percentage = percentage
+    }
+
+    func impactToString() -> String {
+        switch factor {
+            case .Carbon: return "Carbon Emissions Reduced"
+            case .Water: return "Water Saved"
+            case .Soil: return "Soil Quality Improved"
+            case .Antibiotics: return "Antibiotics Mitigated"
+        }
+    }
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                print("Impact: \(percentage)")
+            }) {
+                Text(impactToString())
             }
         }
     }
