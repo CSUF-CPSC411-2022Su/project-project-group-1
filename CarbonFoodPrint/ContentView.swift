c6300f571aeb91d0d6641f52c66b76b63237f9df
Jonathan Story <jstory@csu.fullerton.edu>
2022-06-14 12:54:16 -0700

    Updated with modifiers for borders

--- CarbonFoodPrint/ContentView.swift
+++ CarbonFoodPrint/ContentView.swift
@@ -14,11 +14,14 @@ struct ContentView: View {
         
         
         HStack {
+            //Text("TEST")
+                //.modifier(ContentText())
+            
             Button(action: {
             print("Was clicked")
             }) {
             Text("MENU")
-            }.padding().border(Color.black)
+            }.padding().modifier(ContentText())  //.border(Color.black)
             
         }
         
@@ -27,12 +30,12 @@ struct ContentView: View {
                 print("Was clicked")
             }) {
                 Text("SCAN MODE")
-            }.padding().border(Color.black)
+            }.padding().modifier(ContentText())   //.border(Color.black)
             Button(action: {
                 print("Was clicked")
             }) {
                 Text("AR MODE")
-            }.padding().border(Color.black)
+            }.padding().modifier(ContentText())   //.border(Color.black)
         }
     }
 }
@@ -42,3 +45,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct ContentText: ViewModifier {
+   func body(content: Content) -> some View {
+        content
+           .font(.custom("Courier New", size: 30))
+           .foregroundColor(Color.white)
+           .padding()
+                      .background(Color.black)
+                      .cornerRadius(10)
+               }
+           }
