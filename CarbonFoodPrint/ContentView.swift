a2679ccf8ade1e886ecbc8c8895c585245c1a5e8
britney-py <71041421+britney-py@users.noreply.github.com>
2022-06-20 00:15:25 -0700

    updated DailyWeeklyReport & still fixing ContentView

--- CarbonFoodPrint/ContentView.swift
+++ CarbonFoodPrint/ContentView.swift
@@ -8,21 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        TabView
+    @State var breakfast: String = ""
+    @State var lunch: String = ""
+    @State var dinner: String = ""
+    @State var transportation: String = ""
+    @State var milesDriven: String = ""
+    
+    var body: some View
+    {
+        VStack
         {
-            //since i want to do tab view, make sure each
-            //question can be seen with each swipe
-            Text("First Question")
-            Text("This can be second ?")
-            Text("Third Question")
+            Text("Daily Questions")
+                .font(.headline)
+//                .font(.custom("Courier New", size: 20))
+//                .padding()
+            HStack
+            {
+                Spacer()
+                Text("What did you eat for breakfast? ")
+                TextField("", text: $breakfast)
+            }
+            HStack
+            {
+                Spacer()
+                Text("What did you eat for lunch? ")
+                TextField("", text: $lunch)
+            }
+            HStack
+            {
+                Spacer()
+                Text("What did you eat for dinner? ")
+                TextField("", text: $dinner)
+            }
+//            NavigationLink(destination: MealCategory()) {
+//                Text("Click here to fill out Breakfast, Lunch, and Dinner!")
+//            }
+            HStack
+            {
+                Spacer()
+                Text("What was your source of transporation? ")
+                TextField("", text: $transportation)
+                Spacer()
+            }
         }
-//        Text("Hello from Weekly!!").padding()
-        .tabViewStyle(.page)
-        .indexViewStyle(.page(backgroundDisplayMode: .always))
-        DailyReport()
+        Spacer()
+        VStack
+        {
+            Text(breakfast)
+            Text(lunch)
+            Text(dinner)
+            Text(transportation)
+        }
     }
 }
+        
+        
+        
+//        TabView
+//        {
+//            //since i want to do tab view, make sure each
+//            //question can be seen with each swipe
+//            Text("First Question")
+//            Text("This can be second ?")
+//            Text("Third Question")
+//        }
+//        Text("Hello from Weekly!!").padding()
+//        .tabViewStyle(.page)
+//        .indexViewStyle(.page(backgroundDisplayMode: .always))
+//        DailyReport()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- CarbonFoodPrint/DailyWeeklyReport.swift
+++ CarbonFoodPrint/DailyWeeklyReport.swift
@@ -8,43 +8,144 @@
 import Foundation
 import SwiftUI
 
-// I want the daily report to be a scroll page
-struct WeeklyReport: View {
-    var body: some View {
-            VStack {
-                Text("Daily/Weekly Report").padding()
-        }
-    }
-}
+// 6.2 miles produces 1.97 kg CO2
+// REFERENCE: https://www.co2everything.com/co2e-of/toyota-corolla-2020
 
-// Daily Report for Users
-struct DailyReport: View {
-    @State var foods: String = ""
-    // consider doing a list for foods so the user can choose instead of having to type
-    // the foods out (more user friendly)
-    @State var milesDriven: String = ""
-    // convert to Int unless you want to do list so the user can choose ?
-    // or maybe Str would work best because the user might have taken the bus and not
-    // know how many miles they drove
-    @State var transportation: String = ""
-    // allow user to type
-
+class totalCO2
+{
+    var carType: String
+    var CO2 : Double
+    var milesDriven: Double
     
-    var body: some View {
-        Text("Daily and Weekly Report").padding() //move this to the top
-        VStack{
-            HStack{
-                Text("What did you eat today?")
-                TextField("Choose from the list below: ", text: $foods) //make these centered
-            }
-            HStack{
-                Text("How many miles did you drive today? ")
-                TextField("Miles: ", text: $milesDriven)
-            }
-            HStack{
-                Text("What was your source of transportation today? ")
-                TextField("Source of transportation: ", text: $transportation)
-            }
+    // initializer
+    init(carType: String, CO2: Double, milesDriven: Double)
+    {
+        self.carType = carType
+        self.CO2 = CO2
+        self.milesDriven = milesDriven
+    }
+    
+    func carDrivenDaily()
+    {
+        // assumming the user only uses these four cars
+        
+        // maybe a dictionary would be better ["toyota corolla": 4.34] <- use the value, multiply value * milesDriven
+        // you should get total CO2 for the day
+        if carType.contains("Toyota Corolla")
+        {
+            // for every 6.2 miles driven, 4.34 lbs of co2 is produced
+            return CO2 = 4.34
+        }
+        if carType.contains("Tesla Model Y")
+        {
+            return CO2 = 1.92
+        }
+        if carType.contains("Nissan Leaf")
+        {
+            return CO2 = 2.18
+        }
+        if carType.contains("BMW X5")
+        {
+            return CO2 = 8.20
         }
     }
+    
+//    func totalC02(milesDriven: Double) -> Double
+//    {
+//        let received_C02: function
+//        received_C02 = carDrivenDaily
+//        return milesDriven * received_C02
+//        
+//    }
 }
+    
+
+
+//
+//// I want the daily report to be a scroll page
+//struct WeeklyReport: View {
+//    var body: some View {
+//            VStack {
+//                Text("Daily Questions")
+//        }
+//    }
+//}
+//
+//// Daily Report for Users
+//struct DailyReport: View {
+//    @State var eatToday: String = ""
+//    // consider doing a list for foods so the user can choose instead of having to type
+//    // the foods out (more user friendly)
+//    @State var milesDriven: String = ""
+//    // convert to Int unless you want to do list so the user can choose ?
+//    // or maybe Str would work best because the user might have taken the bus and not
+//    // know how many miles they drove
+//    @State var transportation: String = ""
+//    // allow user to type
+//
+//
+//    var body: some View {
+//        Text("Daily Questions").padding() //move this to the top
+//
+//
+//        VStack
+//        {
+//            Text("Daily Questions")
+//                .font(.headline)
+//                .font(.custom("Courier New", size: 20))
+//                .padding()
+//            HStack
+//            {
+//                Spacer()
+//                Text("What did you eat today? ")
+//                TextField("", text: $eatToday)
+//            }
+//            HStack
+//            {
+//                Spacer()
+//                Text("What was your source of transporation? ")
+//                TextField("", text: $transportation)
+//                Spacer()
+//            }
+//        }
+//        Spacer()
+//        VStack
+//        {
+//            Text(eatToday)
+//            Text(transportation)
+//        }.background(Color.green)
+//        TabView
+//        {
+//            //since i want to do tab view, make sure each
+//            //question can be seen with each swipe
+//            Text("First Question")
+//            Text("This can be second ?")
+//            Text("Third Question")
+//        }
+//        Text("Hello from Weekly!!").padding()
+//        .tabViewStyle(.page)
+//        .indexViewStyle(.page(backgroundDisplayMode: .always))
+//        DailyReport()
+//    }
+//}
+        
+        
+        
+        
+        
+//        VStack{
+//            HStack{
+//                Text("What did you eat today?")
+//                TextField("Choose from the list below: ", text: $foods) //make these centered
+//            }
+//            HStack{
+//                Text("How many miles did you drive today? ")
+//                TextField("Miles: ", text: $milesDriven)
+//            }
+//            HStack{
+//                Text("What was your source of transportation today? ")
+//                TextField("Source of transportation: ", text: $transportation)
+//            }
+//        }
+//    }
+//}
