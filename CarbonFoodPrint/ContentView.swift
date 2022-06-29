a337479a62cf14d04e1908c885e81d84173f2792
Jonathan Story <jstory@csu.fullerton.edu>
2022-06-29 02:58:31 -0700

    Moved Everything out of ContentView

--- CarbonFoodPrint/ContentView.swift
+++ CarbonFoodPrint/ContentView.swift
@@ -8,97 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    //@StateObject var volunteer = Volunteer(name: "", age: 0)
-    @State var TheVar: String = "*This is the string variable passed from the first page*"
-    //@State var TheBunch = ["Banana": 234, "Apple": 347, "Orange": 21 ]
-    @State var TheList: String = "Info goes here"
-    //@State var age: String = ""
-    //@State var message: String = ""
-   // @State var TheChoice: Int = 0
-    @StateObject var man = ProductManager()
-    @State var cost: Int = 0
-    
-    //@State let dict: [String: Int] =
     var body: some View {
-        //Text("Hello, world!")
-            //.padding()
-        
-        
-        HStack {
-            //Text("TEST")
-                //.modifier(ContentText())
-            
-            Button(action: {
-            print("Was clicked")
-            }) {
-            Text("MENU")
-            }.padding().modifier(ContentText())  //.border(Color.black)
-            
-        }
         TabView {
-            TotalListViewer(cost: $cost)
-                .tabItem {
-                    Image(systemName: "info")
-                    Text("LIST OF ITEMS")
-                }
-            BarView(TheVar: $TheVar, TheList: $TheList, cost: $cost)
-                .tabItem {
-                    Image(systemName: "camera")
-                    Text("BARCODE SCAN")
-                }
-            //CrossWalkInfo()
-              //  .tabItem {
-                //    Image(systemName: "info")
-                  //  Text("Crosswalk Info")
-                //}
-            //AddCrossWalk()
-              //  .tabItem {
-                //    Image(systemName: "plus")
-                  //  Text("Add Crosswalk")
-               // }
-        }.environmentObject(man)
-        
-        /*
-        NavigationView {
-        VStack {
-            Button(action: {
-                print("Was clicked")
-            }) {
-                Text("SCAN MODE").bold()
-            }.padding().modifier(ContentText())   //.border(Color.black)
-            Button(action: {
-                print("Was clicked")
-            }) {
-                Text("AR MODE")
-            }.padding().modifier(ContentText())   //.border(Color.black)
-            
-            NavigationLink(destination: VolunteerDetails(TheVar: $TheVar)) {
-                                    Text("See Notices")
-                                         .font(.caption)
-                            }//.padding(.bottom, 30)
+            BarcodeFeature()
         }
-    }
-         */
-    }
-        
+    
 }
-
-
-
-struct Information: View {
-    @Binding var TheVar: String
-    var body: some View {
-        Text(TheVar).bold().padding()
-    }
 }
-
-struct ListInfo: View {
-    @Binding var TheList: String
-    var body: some View {
-        Text(TheList).bold().padding()
-    }
-}
- 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -106,101 +22,9 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct ContentText: ViewModifier {
-   func body(content: Content) -> some View {
-        content
-           .font(.custom("Courier New", size: 30))
-           .foregroundColor(Color.white)
-           .padding()
-                      .background(Color.black)
-                      .cornerRadius(10)
-               }
-           }
-
-struct VolunteerDetails: View {
-    @Binding var TheVar: String
-    var body: some View {
-        VStack {
-            Text("This scanner is still being implemented.")
-                .font(.headline)
-                .padding(.bottom, 30)
-            HStack {
-                Text("Notice Page.")
-                
-                Spacer()
-            }
-            HStack {
-                //Spacer()
-                //Text(TheVar).bold().padding() //.frame(width: 100) //.padding(.bottom, 10)
-                Information(TheVar: $TheVar).padding()
-                //Text("Ages 18 - 50:")
-                //Text("6 hours").bold()
-            }
-
-            HStack {
-                //Text("Ages 51 - 60:")
-                //Text("3 hours").bold()
-            }//.padding(.bottom, 30)
-            HStack {
-                //Text("*People below 13 and over 60 are not eligible to volunteer.")
-                    //.font(.caption)
-            }
-            Spacer()
-        }
-    }
-}
-
-struct ListViewer: View {
-    @Binding var TheList: String
-    @EnvironmentObject var man: ProductManager
-    //@State var TheChoice: Int
-    var body: some View {
-        VStack {
-            Text("Information for Items")
-                .font(.headline)
-                .padding(.bottom, 30)
-            HStack {
-                Text("Banana:")
-                
-                Spacer()
-            }
-            HStack {
-                //Text(products.name)
-                  //  .font(.largeTitle)
-                //Text(crosswalk.description)
-                //  .font(.caption)
-                //Spacer()
-                //Text(TheVar).bold().padding() //.frame(width: 100) //.padding(.bottom, 10)
-                //ListInfo(TheList: $TheList).padding()
-                
-                //Text("Ages 18 - 50:")
-                //Text("6 hours").bold()
-                /*
-                ForEach(man.products) {
-                    product in
-                    VStack (alignment: .leading) {
-                        Text(product.name)
-                            .font(.largeTitle)
-                        Text(product.description)
-                            .font(.caption)
-                    }
-                }
-                */
-            }
-            Spacer()
-        }
-    }
-}
 
 
 
-struct ItemView: Identifiable {
-    /// The Identifiable protocol requires an id property that should be a unique value
-    /// UUID generates a unique random hexadecimal string
-    var id = UUID()
-    var productName: String
-    var description: String
-}
 
 
 
