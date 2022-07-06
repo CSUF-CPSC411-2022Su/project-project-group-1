638f27b85e79e11283b87334f59552bf22d49f13
Randy Nguyen <43362494+randyxnguyen77@users.noreply.github.com>
2022-07-06 16:21:40 -0700

    Added GreenMaps Class

---
+++ CarbonFoodPrint/GreenMaps.swift
@@ -0,0 +1,57 @@
+//
+//  GreenMaps.swift
+//  CarbonFoodPrint
+//
+//  Created by Nguyen, Randy on 6/27/22.
+//
+
+import Foundation
+
+class CurrentLocation: ObservableObject {
+    @Published var country: String
+    @Published var state: String
+    @Published var city: String
+    @Published var zipCode: String
+    @Published var withinRange: Int
+    
+    var validPrint: String {
+        var list: String = ""
+        if !country.isEmpty, !state.isEmpty, !city.isEmpty, !zipCode.isEmpty {
+            list = country + " " + state + " " + city + " " + zipCode + " " + String(withinRange)
+                + "\n is a valid location"
+        } else {
+            list = "Invalid Location"
+        }
+        return list
+    }
+    var toMeters: Int {
+        var meters: Int = 0
+        meters = Int(Double(withinRange) * 1609.33)
+        return meters
+    }
+    
+    init(country: String, state: String, city: String, zipCode: String, withinRange: Int) {
+        self.country = country
+        self.state = state
+        self.city = city
+        self.zipCode = zipCode
+        self.withinRange = withinRange
+    }
+    
+    init() {
+        self.country = ""
+        self.state = ""
+        self.city = ""
+        self.zipCode = ""
+        self.withinRange = 0
+    }
+}
+
+class LocationManager: ObservableObject {
+    @Published var locations: [CurrentLocation] = []
+    
+    init() {
+
+    }
+    
+}
